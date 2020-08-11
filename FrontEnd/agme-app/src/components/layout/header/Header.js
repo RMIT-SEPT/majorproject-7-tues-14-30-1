08daee2956550261d83ec267018d886edbed45cd
Matt Meskell <s3385819@student.rmit.edu.au>
2020-08-12 09:45:39 +1000

    generation of front-end framework using React. Includes header, footer, and dashboard templates.

---
+++ FrontEnd/agme-app/src/components/layout/header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                This is the header.
+            </div>
+        )
+    }
+}
+
+export default Header;
