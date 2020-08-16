4729c115de296632982e743fafc82f99a5f09f35
ryan wilson <s3716816@student.rmit.edu.au>
2020-08-16 14:46:03 +1000

    setup framerwork for testing

---
+++ BackEnd/src/test/java/tests/BusinessTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setPhone_number() {
+    }
+}
