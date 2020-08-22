7c5b0e091643bfaf977473c85fafd65774abc53e
Matt Meskell <s3385819@student.rmit.edu.au>
2020-08-22 11:48:28 +1000

    created top navbar and footer, with styling. updated dependencies.

--- FrontEnd/agme-app/src/components/layout/footer/Footer.js
+++ FrontEnd/agme-app/src/components/layout/footer/Footer.js
@@ -1,13 +1,109 @@
 import React, { Component } from 'react'
+import './Footer.css'
+import styled from 'styled-components'
 
 class Footer extends Component {
     render() {
         return (
-            <div>
-                This is the footer.
-            </div>
+            <FooterContainer className="main-footer">
+
+                <div className='footer-middle'>
+            
+                    <div className='container'>
+                        
+                        <div className='row'>
+                    
+                        
+                            <div className='col-md-3 col-sm-6'>     
+                            
+                                <h4> Development Team </h4>
+                            
+                                <ul className='list-unstyled'>
+                                    <li>Erik Olivejoe</li>
+                                    <li>Matt Meskell</li>
+                                    <li>Prerak Soni</li>
+                                    <li>Ryan Wilson</li>
+                                </ul>
+                            </div>
+
+                            <div className='col-md-3 col-sm-6'>     
+                            
+                                <h4> Site Navigation </h4>
+                            
+                                <ul className='list-unstyled'>
+                                    <li><a href="/">My Appointments</a></li>
+                                    <li><a href="/">Search Businesses</a></li>
+                                    <li><a href="/">Link 3</a></li>
+                                    <li><a href="/">Link 4</a></li>
+                                </ul>
+                            </div>
+
+                            <div className='col-md-3 col-sm-6'>     
+                            
+                                <h4> Project Links </h4>
+                            
+                                <ul className='list-unstyled'>
+                                    <li><a href="/">GitHub</a></li>
+                                    <li><a href="/">Notion</a></li>
+                                    <li><a href="/">MS Teams</a></li>
+                                </ul>
+                            </div>
+
+                            <div className='col-md-3 col-sm-6'>     
+                            
+                                <h4> Other </h4>
+                            
+                                <ul className='list-unstyled'>
+                                    <li><a href="/">Whales</a></li>
+                                    <li><a href="/">The Moon</a></li>
+                                    <li><a href="/">Egg</a></li>
+                                </ul>
+                            </div>
+
+
+                            
+                        </div>
+
+                        
+                    
+                        <div className='footer-bottom'>
+                            <p className='text-xs-center'> &copy;{new Date().getFullYear()} AGME App </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            
+            </FooterContainer>
         )
     }
 }
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+
+    text-align: center;
+    justify-content: center;
+    
+
+    .footer-middle {
+        background: var(--mainDark);
+        padding-top: 3rem;
+        color: var(--mainWhite);
+    }
+
+    .footer-bottom {
+        padding-top: 3rem;
+        padding-bottom: 2rem;
+    }
+
+    ul li a {
+        color: var(--mainGrey);
+    }
+
+    ul li a:hover {
+        color: var(--mainLightGrey);
+    }
+
+`;
