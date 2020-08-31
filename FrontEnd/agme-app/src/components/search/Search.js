b88c1682f9a82362214285722871d88e9cebf0a3
Matt Meskell <s3385819@student.rmit.edu.au>
2020-08-31 15:55:30 +1000

    pulled most of the unecessary stuff out of search, and added placeholder data

--- FrontEnd/agme-app/src/components/search/Search.js
+++ FrontEnd/agme-app/src/components/search/Search.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Loader from './loader.gif'
 import './Search.css'
-import PageNavigation from '../pageNavigation/PageNavigation'
+import { Table } from 'react-bootstrap'
+
+
 
 class Search extends Component {
 
@@ -13,28 +15,17 @@ class Search extends Component {
             results: {},
             loading: false,
             message: '',
-            totalResults: 0,
-    		totalPages: 0,
-	    	currentPageNo: 0,
         };
         
         this.cancel = '';
 
     }
 
-    /**
-     * Fetch the search results and update the state with the result.
-     *
-     * @param {int} updatedPageNo Updated Page No.
-     * @param {String} query Search Query.
-     *
-     */
-
+    // Fetch the search results and update the state with the result.
     fetchSearchResults = (updatedPageNo = '', query ) => {
 
         const pageNumber = updatedPageNo ? `&page=${updatedPageNo}` : '';        
         const searchUrl = `https://pixabay.com/api/?key=12413278-79b713c7e196c7a3defb5330e&q=${query}${pageNumber}`;
-
         
         if (this.cancel) {
             // Cancel the previous request before making a new request
@@ -49,16 +40,12 @@ class Search extends Component {
                 cancelToken: this.cancel.token,
             })
             .then((res) => {
-                const total = res.data.total;
-		        const totalPagesCount = this.getPagesCount( total, 20 );
                 const resultNotFoundMsg = !res.data.hits.length
                     ? 'There are no more search results. Please try a new search.'
                     : '';
                 this.setState({
                     results: res.data.hits,
                     totalResults: res.data.total,
-			        currentPageNo: updatedPageNo,
-			        totalPages: totalPagesCount,
                     message: resultNotFoundMsg,
                     loading: false,
                 });
@@ -75,14 +62,8 @@ class Search extends Component {
     
     render() {
 
-        const { query, loading, message, currentPageNo, totalPages } = this.state;
+        const { query, loading, message } = this.state;
         
-        // showPrevLink will be false, when on the 1st page, hence Prev link be shown on 1st page.
-        const showPrevLink = 1 < currentPageNo;
-        
-        // showNextLink will be false, when on the last page, hence Next link wont be shown last page.
-        const showNextLink = totalPages > currentPageNo;
-
         return (
 
             <div className="container">
@@ -91,43 +72,26 @@ class Search extends Component {
             <h2 className="heading">AGME: Search Businesses</h2>
             
             { /* Search Input */ }
-            <label className="search-label" htmlFor="search-input">
-                <input
+
+            <div class="md-form active-pink active-pink-2 mb-3 mt-0">
+            <input
                     type="text"
                     value={query}
                     id="search-input"
                     placeholder="Search..."
                     onChange={this.handleOnInputChange}
                 />
-                <i className="fa fa-search search-icon"/>
-            </label>
+            </div>
 
             {/*Error Message*/}
             { message && <p className="message">{message}</p> }
             
             {/*Loader*/}
-            <img  src={Loader} className={`search-loading ${loading ? 'show' : 'hide' }`}  alt="loader"/>   
-
-            {/*Navigation Top*/}
-            <PageNavigation
-            loading={loading}
-            showPrevLink={showPrevLink}
-            showNextLink={showNextLink}
-            handlePrevClick={() => this.handlePageClick('prev')}
-            handleNextClick={() => this.handlePageClick('next')}
-            />
+            <img  src={Loader} className={`search-loading ${loading ? 'show' : 'hide' }`}  alt="loader"/>
             
             {/*Result*/}
             { this.renderSearchResults() }
-
-            {/*Navigation Bottom*/}
-            <PageNavigation
-            loading={loading}
-            showPrevLink={showPrevLink}
-            showNextLink={showNextLink}
-            handlePrevClick={() => this.handlePageClick('prev')}
-            handleNextClick={() => this.handlePageClick('next')}
-            />
+            
             
         </div>
         )
@@ -150,55 +114,46 @@ class Search extends Component {
         const {results} = this.state;
         if (Object.keys(results).length && results.length) {
             return (
-                <div className="results-container">
-                    {results.map((result) => {
-                        return (
-                            <a key={result.id} href={result.previewURL} className="result-items">
-                                <h6 className="image-username">{result.user}</h6>
-                                <div className="image-wrapper">
-                                    <img className="image" src={result.previewURL} alt={result.user}/>
-                                </div>
-                            </a>
-                        );
-                    })}
-                </div>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>Business Name</th>
+                    <th>Phone</th>
+                    <th>Employees</th>
+                    <th>Next Available Appointment</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                    <td>1</td>
+                    <td>Best Haircuts Studio</td>
+                    <td>0476 342 563</td>
+                    <td>4</td>
+                    <td>3:00pm Tuesday</td>
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    <td>Curl Up & Dye</td>
+                    <td>0433 555 096</td>
+                    <td>2</td>
+                    <td>9:00am, Wednesday</td>
+                    </tr>
+                    <tr>
+                    <td>3</td>
+                    <td>Shaving The Day</td>
+                    <td>0457 398 287</td>
+                    <td>9</td>
+                    <td>9:00am Monday</td>
+                    </tr>
+                </tbody>
+            </Table>
+
             );
         }
     };
 
-    /**
-     * Fetch results according to the prev or next page requests.
-     *
-     * @param {String} type 'prev' or 'next'
-     */
-    handlePageClick = (type) => {
-        
-        const updatedPageNo =
-                'prev' === type
-                    ? this.state.currentPageNo - 1
-                    : this.state.currentPageNo + 1;
-        if (!this.state.loading) {
-            this.setState({ loading: true, message: '' }, () => {
-                // Fetch previous 20 Results
-                this.fetchSearchResults(updatedPageNo, this.state.query);
-            });
-        }
-
-        return false;
-    };
-
-
-    /**
-     * Get the Total Pages count.
-     *
-     * @param total
-     * @param denominator Count of results per page
-     * @return {number}
-     */
-    getPagesCount = (total, denominator) => {
-        const divisible = total % denominator === 0;
-        const valueToBeAdded = divisible ? 0 : 1;
-        return Math.floor(total / denominator) + valueToBeAdded;
-    };
 
 } export default Search;
