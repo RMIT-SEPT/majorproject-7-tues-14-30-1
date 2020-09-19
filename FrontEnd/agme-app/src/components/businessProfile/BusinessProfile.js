b038cc733560314f179c8ec31f47785b20c81f15
Matt Meskell <s3385819@student.rmit.edu.au>
2020-09-19 14:23:58 +1000

    made business profile display list of employees

--- FrontEnd/agme-app/src/components/businessProfile/BusinessProfile.js
+++ FrontEnd/agme-app/src/components/businessProfile/BusinessProfile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
 class BusinessProfile extends Component {
 
@@ -10,21 +11,40 @@ class BusinessProfile extends Component {
             name: '',
             phone_number: '',
             email: '',
-            cheapestCost: ''
+            cheapestCost: '',
+            employees: {}
         };
 
+        this.cancel = '';
         console.log(this.state)
     }
 
+    
+
     componentDidMount() {
         
-        const request = `http://localhost:7000/api/business?id=${this.state.business_id}`;
-        
+        this.fetchBusinessData();
+        this.fetchEmployeeData();
+    }
 
-        axios.get(request)
+    toTitleCase = (phrase) => {
+        return phrase
+          .toLowerCase()
+          .split(' ')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+      };
+
+    fetchBusinessData = () => {
+
+        const businessRequest = `http://localhost:7000/api/business?id=${this.state.business_id}`;
+
+        axios.get(businessRequest)
         .then((response) => {
+            console.log('response.data:')
             console.log(response.data)
             const business = response.data.payload;            
+            console.log('business:')
             console.log(business)
 
             this.setState({
@@ -34,6 +54,8 @@ class BusinessProfile extends Component {
                 cheapestCost: business.cheapestCost
             })
         }).catch((error) => {
+            console.log('error:')
+            console.log(error)
                 this.setState({
                     name: 'ERROR: Business does not exist :(',
                     phone_number: '',
@@ -41,16 +63,87 @@ class BusinessProfile extends Component {
                     cheapestCost: ''
             })
         });
+
+    }
+
+    fetchEmployeeData = () => {
+        
+        const employeeRequest = `http://localhost:7000/api/business/getEmployees?business_id=${this.state.business_id}`
+
+        axios.post(employeeRequest)
+        .then((response) => {
+            console.log('response.data:')
+            console.log(response.data)
+            this.setState({employees: response.data})
+        }).catch((error) => {
+            console.log('error:')
+            console.log(error)
+        });
+    
+    }
+
+    renderEmployees = () => {
+        
+        const results = this.state.employees.payload;
+        console.log('results:');       
+        console.log(results);   
+
+        if (results) {
+            console.log('results query test'); 
+            console.log(results[0]); 
+
+            const rows = results.map(row => 
+                <tr>
+                    <td>{row.first_name} {row.last_name}</td>
+                    <td>{row.phone_number}</td>
+                    <td>{row.email}</td>
+                    <td>{row.cheapest_cost}</td>
+                    <td>Book Appt</td>
+                </tr>)
+
+            return(
+                
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Employee</th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                    <th>Cheapest Cost</th>
+                    <th>Book</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+
+                </Table>
+            )
+        }
+
+ 
+        
+
     }
 
 
     render() {
 
         return(
-            <div>
-                <div>This is a business, yo.</div>
-                <div>Name: {this.state.name}</div>
-            </div>
+
+            <div className="container">
+            
+            { /* Heading: Business Name */ }
+            <h1>{this.toTitleCase(this.state.name)}</h1>
+            {console.log("this.state.name")}
+            {console.log(this.state.name)}
+            
+            
+            <h4 className="heading">Employees</h4>
+
+            { /* List of Employees */ }
+            {this.renderEmployees()}
+        
+        
+        </div>
         )
     
     }
